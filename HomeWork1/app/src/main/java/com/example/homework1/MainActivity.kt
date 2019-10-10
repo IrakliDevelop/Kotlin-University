c4e1f2b57af0ce1150d5633b15a351f6e40462aa
Irakli <i_iremashvili@cu.edu.ge>
2019-10-10 17:40:04 +0400

    bugfix: handling zero division

--- HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
+++ HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
@@ -47,10 +47,14 @@ class MainActivity : AppCompatActivity() {
         val input2 = findViewById<EditText>(R.id.input2)
         return input1.run { text.toString().toInt() } * input2.run { text.toString().toInt()}
     }
-    private fun divide(): Float {
+    private fun divide(): Any {
         val input1 = findViewById<EditText>(R.id.input1)
         val input2 = findViewById<EditText>(R.id.input2)
-        return input1.run { text.toString().toFloat() } / input2.run { text.toString().toFloat()}
+        return try {
+            input1.run { text.toString().toFloat() } / input2.run { text.toString().toFloat()}
+        } catch (e: ArithmeticException) {
+            "Can't divide by zero"
+        }
     }
     private fun passToSecondActivity(operand: String = "divide"){
         val intent = Intent(this@MainActivity, SecondActivity::class.java)
