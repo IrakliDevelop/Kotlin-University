7831a0494f99656599e7441cfc158b36c4c30153
Irakli <i_iremashvili@cu.edu.ge>
2019-10-18 19:41:59 +0400

    implement empty input exception handling

--- HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
+++ HomeWork1/app/src/main/java/com/example/homework1/MainActivity.kt
@@ -13,51 +13,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val input1 = findViewById<EditText>(R.id.input1)
+        val input2 = findViewById<EditText>(R.id.input2)
         val sumButton = findViewById<Button>(R.id.sumButton)
         val subButton = findViewById<Button>(R.id.subButton)
         val multiplyButton = findViewById<Button>(R.id.multiplyButton)
         val divideButton = findViewById<Button>(R.id.divideButton)
         sumButton.setOnClickListener {
-            passToSecondActivity("sum")
+            passToSecondActivity("sum", input1, input2)
         }
         subButton.setOnClickListener {
-            passToSecondActivity("subtract")
+            passToSecondActivity("subtract", input1, input2)
         }
         multiplyButton.setOnClickListener {
-            passToSecondActivity("multiply")
+            passToSecondActivity("multiply", input1, input2)
         }
         divideButton.setOnClickListener {
-            passToSecondActivity()
+            passToSecondActivity("divide", input1, input2)
         }
 
     }
-    private fun sum(): Any {
-        val input1 = findViewById<EditText>(R.id.input1)
-        val input2 = findViewById<EditText>(R.id.input2)
-
-        if(input1.text.isEmpty() || input2.text.isEmpty()){
-            Toast.makeText(this,"asdf",Toast.LENGTH_LONG).show()
-            return 0
-        }
-
+    private fun sum(input1: EditText, input2: EditText): String {
         val result = input1.run { text.toString().toInt() } + input2.run { text.toString().toInt() }
         return "${input1.text} + ${input2.text} = $result"
     }
-    private fun subtract(): String {
-        val input1 = findViewById<EditText>(R.id.input1)
-        val input2 = findViewById<EditText>(R.id.input2)
+    private fun subtract(input1: EditText, input2: EditText): String {
         val result = input1.run { text.toString().toInt() } - input2.run { text.toString().toInt()}
         return "${input1.text} - ${input2.text} = $result"
     }
-    private fun multiply(): String  {
-        val input1 = findViewById<EditText>(R.id.input1)
-        val input2 = findViewById<EditText>(R.id.input2)
+    private fun multiply(input1: EditText, input2: EditText): String  {
         val result = input1.run { text.toString().toInt() } * input2.run { text.toString().toInt()}
         return "${input1.text} * ${input2.text} = $result"
     }
-    private fun divide(): String {
-        val input1 = findViewById<EditText>(R.id.input1)
-        val input2 = findViewById<EditText>(R.id.input2)
+    private fun divide(input1: EditText, input2: EditText): String {
         return try {
             if (input2.run { text.toString().toInt() } == 0) {
                 val exception: ArithmeticException = ArithmeticException("Can't Divide By Zero")
@@ -69,16 +57,23 @@ class MainActivity : AppCompatActivity() {
             "Can't divide by zero"
         }
     }
-    private fun passToSecondActivity(operand: String = "divide"){
-        val intent = Intent(this@MainActivity, SecondActivity::class.java)
-        var result: Any
-        when (operand) {
-            "sum" -> result = sum()
-            "subtract" -> result = subtract()
-            "multiply" -> result = multiply()
-            else -> result = divide()
+    private fun passToSecondActivity(operand: String = "divide", input1: EditText, input2: EditText){
+        try {
+            if(input1.text.isEmpty() || input2.text.isEmpty()){
+                val emptyInputException = EmptyInputException("Empty Input")
+                throw emptyInputException
+            }
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            val result: Any = when (operand) {
+                "sum" -> sum(input1, input2)
+                "subtract" -> subtract(input1, input2)
+                "multiply" -> multiply(input1, input2)
+                else -> divide(input1, input2)
+            }
+            intent.putExtra("result", "$result")
+            startActivity(intent)
+        } catch (e: EmptyInputException) {
+            Toast.makeText(this,"Please fill both inputs",Toast.LENGTH_LONG).show()
         }
-        intent.putExtra("result", "$result")
-        startActivity(intent)
     }
 }
