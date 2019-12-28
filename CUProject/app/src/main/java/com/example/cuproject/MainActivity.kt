35b84f95b0540fe7e21a9185c5eaa8f12538d585
Irakli <i_iremashvili@cu.edu.ge>
2019-12-29 00:04:34 +0400

    initialize recycle view

--- CUProject/app/src/main/java/com/example/cuproject/MainActivity.kt
+++ CUProject/app/src/main/java/com/example/cuproject/MainActivity.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cuproject.dto.movie.Movie
 import com.example.cuproject.dto.movie.MovieResponse
 import com.example.cuproject.services.ApiInterface
 import com.example.cuproject.services.ApiUtils
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,11 +19,16 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var apiService: ApiInterface? = null
-        var spinner: ProgressBar = findViewById(R.id.progressBar1)
+        val spinner: ProgressBar = findViewById(R.id.progressBar1)
+        linearLayoutManager = LinearLayoutManager(this)
+        movieListRecyclerView.layoutManager = linearLayoutManager
+
         spinner.visibility = View.VISIBLE
 
         //After oncreate
@@ -41,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                 spinner.visibility = View.GONE
                 data.value = response.body()!!.movies
                 movieList = data.value
+                // TODO: logic
                 Log.d("retrofit", "success")
                 Log.d("data", "${data.value}")
                 Log.d("list", "${movieList?.get(0)?.cast?.get(0)}")
