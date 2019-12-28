311c1049651e98d626827f392b22c8d92b39f120
Irakli <i_iremashvili@cu.edu.ge>
2019-12-28 18:29:37 +0400

    add loading spinner and create list of movies

--- CUProject/app/src/main/java/com/example/cuproject/MainActivity.kt
+++ CUProject/app/src/main/java/com/example/cuproject/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.cuproject
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import com.example.cuproject.dto.movie.Movie
@@ -18,8 +20,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("traki", "traki")
         var apiService: ApiInterface? = null
+        var spinner: ProgressBar = findViewById(R.id.progressBar1)
+        spinner.visibility = View.VISIBLE
 
         //After oncreate
 
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
 
         val fetch = apiService.fetchAll()
         val data = MutableLiveData<List<Movie>>()
+        var movieList: List<Movie>? = null
 
         fetch.enqueue(object : Callback<MovieResponse> {
             override fun onFailure(call: Call<MovieResponse>?, t: Throwable?) {
@@ -34,9 +38,12 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                spinner.visibility = View.GONE
                 data.value = response.body()!!.movies
+                movieList = data.value
                 Log.d("retrofit", "success")
                 Log.d("data", "${data.value}")
+                Log.d("list", "${movieList?.get(0)?.cast?.get(0)}")
             }
         })
 
