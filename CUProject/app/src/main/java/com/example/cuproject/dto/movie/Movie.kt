92de97e72c75cc340468e97b11c7bd8696ec333c
Irakli <i_iremashvili@cu.edu.ge>
2020-01-05 16:56:43 +0400

    add serialized name properties to data classes

--- CUProject/app/src/main/java/com/example/cuproject/dto/movie/Movie.kt
+++ CUProject/app/src/main/java/com/example/cuproject/dto/movie/Movie.kt
@@ -1,20 +1,34 @@
 package com.example.cuproject.dto.movie
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieResponse(
+    @SerializedName("movies")
     val movies: List<Movie>
 )
 
 data class Movie(
+    @SerializedName("id")
     val id: Number,
+    @SerializedName("title")
     val title: String,
+    @SerializedName("date")
     val date: String,
+    @SerializedName("language")
     val language: String,
+    @SerializedName("seasons")
     val seasons: Number,
+    @SerializedName("imageUrl")
     val imageUrl: String,
+    @SerializedName("cast")
     val cast: List<Cast>)
 
 data class Cast(
+    @SerializedName("id")
     val id: Number,
+    @SerializedName("fullName")
     val fullName: String,
+    @SerializedName("role")
     val role: String,
+    @SerializedName("imageUrl")
     val imageUrl: String)
